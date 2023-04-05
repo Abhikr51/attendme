5adb84cf9d49627479b1d9bfcf34b3695cd352f9
satya-kr <satyajit.kr.prajapati@gmail.com>
2023-04-05 11:24:14 +0530

    'renamed_to_attend_me'

--- android/app/src/main/java/com/itzabhi/attendme/MainApplication.java
+++ android/app/src/main/java/com/itzabhi/attendme/MainApplication.java
@@ -1,4 +1,4 @@
-package com.attscanner;
+package com.itzabhi.attendme;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/AttendmeTests/AttendmeTests.m
+++ ios/AttendmeTests/AttendmeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface attscannerTests : XCTestCase
+@interface AttendmeTests : XCTestCase
 
 @end
 
-@implementation attscannerTests
+@implementation AttendmeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
